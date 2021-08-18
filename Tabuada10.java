876401a4c5ccce381b750d05a3292828e89ee632
JoaopLobo <89166337+JoaopLobo@users.noreply.github.com>
2021-08-18 20:01:48 -0300

    Add files via upload

---
+++ Tabuada10.java
@@ -0,0 +1,20 @@
+/**
+ * 0050015129 - João Pedro Barbosa Lobo
+*/
+
+public class Tabuada10{
+   public static void main(String[] args){
+
+      int j = 10;
+      int aux = 0;
+      System.out.println("Tabuada de " + j);
+
+      for (int i = 0; i <= 10; i++){
+         aux = j * i;
+         System.out.print( j + " x " + i + " " + "=");
+         System.out.println(" "+aux);
+      }
+
+   }
+   
+}
